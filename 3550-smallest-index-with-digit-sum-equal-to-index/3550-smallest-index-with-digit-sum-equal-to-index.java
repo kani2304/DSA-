fb77d8be43f924cea2082394cd3cbc7b42eda179
Kanishk Giri <kanishk4020@gmail.com>
2026-09-24 16:14:42 +0530

    Time: 1 ms (99.69%), Space: 45.4 MB (77.88%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int smallestIndex(int[] nums) {
+        int indx =-1;
+        int n =nums.length;
+
+        for(int i=0; i <n; i++){
+            int sum=0;
+
+            while(nums[i] > 0){
+                int digit =nums[i]%10;
+                sum += digit;
+                nums[i] =nums[i]/10;
+            }
+
+            if(sum == i){
+                indx = i;
+                return indx;
+            }
+        }
+
+        return indx;
+    }
+}
